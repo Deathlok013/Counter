bcdae35d2eb3861755807493554332012825dc45
Alexandr <petrov.alex23@proton.me>
2023-09-05 22:15:39 +0300

    Исправлено scrollView в соответствии с camelCase. Убраны переменные shouldReset и history. Добавлена приватность функциям, свойствам

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,92 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var shouldReset: Bool = false {
-        didSet {
-            /// При каждом обновлении значения переменной - обновляем текст в лэйбле
-            updateCounterLabel()
-        }
-    }
-    var counter: Int = 0 {
-        didSet {
-            /// При каждом обновлении значения переменной - обновляем текст в лэйбле
-            updateCounterLabel()
-        }
-    }
     
-    @IBOutlet var ScrollView: UIScrollView!
-    @IBOutlet var historyView: UITextView!
-    @IBOutlet weak var restartButton: UIButton!
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-
+    private var counter: Int = 0 {
+        didSet {
+            /// При каждом обновлении значения переменной - обновляем текст в лэйбле
+            updateCounterLabel()
+        }
+    }
+    private func updateCounterLabel() {
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    @IBOutlet weak private var scrollView: UIScrollView!
+    @IBOutlet weak private var historyView: UITextView!
+    @IBOutlet weak private var restartButton: UIButton!
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         historyView.isEditable = false
-        historyView.addSubview(ScrollView)
-        ScrollView.contentSize = historyView.bounds.size
-//      Включение прокрутки по контенту
+        historyView.addSubview(scrollView)
+        scrollView.contentSize = historyView.bounds.size
+        // Включение прокрутки по контенту
         historyView.isScrollEnabled = true
-        ScrollView.isScrollEnabled = true
+        scrollView.isScrollEnabled = true
         // Включение прокрутки scrollView
-        ScrollView.alwaysBounceVertical = true
-        historyView.inputView = UIView()
+        scrollView.alwaysBounceVertical = true
+        historyView.text = "История изменений:\n"
     }
     
-    @IBAction func plusTapped(_ sender: Any) {
+    @IBAction private func plusTapped(_ sender: Any) {
         counter += 1
         // Обновляем историю
         let formatter = DateFormatter()
         formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         let dateString = formatter.string(from: Date())
-        history += "\(dateString): значение изменено на +1\n"
+        historyView.text += "\(dateString): значение изменено на +1\n"
         
-        // Отображаем историю в UITextView
-        historyView.text = history
         updateCounterLabel()
     }
-    @IBAction func minusTapped(_ sender: Any) {
+    @IBAction private func minusTapped(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             // Обновляем историю
             let formatter = DateFormatter()
             formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
             let dateString = formatter.string(from: Date())
-            history += "\(dateString): значение изменено на -1\n"
+            historyView.text += "\(dateString): значение изменено на -1\n"
         } else {
             
-                let formatter = DateFormatter()
-                formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-                let dateString = formatter.string(from: Date())
-                history += "\(dateString): попытка уменьшить значение счетчика ниже 0\n"
-                
-                // Отображаем историю в UITextView
-                historyView.text = history
-                return
-            
-        }
-        // Отображаем историю в UITextView
-        historyView.text = history
-        updateCounterLabel()
-    }
-    @IBAction func resetGame(_ sender: Any) {
-        shouldReset = true
-        if shouldReset == true {
-            counter = 0
             let formatter = DateFormatter()
             formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
             let dateString = formatter.string(from: Date())
-            history += "\(dateString): значение сброшено\n"
+            historyView.text += "\(dateString): попытка уменьшить значение счетчика ниже 0\n"
             
-            // Отображаем историю в UITextView
-            historyView.text = history
+            return
         }
+        updateCounterLabel()
     }
-    func updateCounterLabel() {
-        counterLabel.text = "Значение счетчика: \(counter)"
+    
+    @IBAction private func resetGame(_ sender: Any) {
+        counter = 0
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = formatter.string(from: Date())
+        historyView.text += "\(dateString): значение сброшено\n"
     }
-    var history = "История изменений:\n"
-    }
+}
 
